1ac9203702bcecdc2ba8fe921505c3fbd02fc5f8
efekahveci <efekhvci@hotmail.com>
2022-05-15 13:31:20 +0300

    relase

---
+++ gRPCProductClient/ProductClient.cs
@@ -0,0 +1,72 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using ProductProto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gRPCProductClient
+{
+    internal class ProductClient
+    {
+    
+        private GrpcChannel grpcChannel;
+        private Product.ProductClient productClient;
+
+        public ProductClient()
+        {
+            grpcChannel = GrpcChannel.ForAddress("http://localhost:5128");
+            productClient = new Product.ProductClient(grpcChannel);
+        }
+
+        internal ProductModel GetProduct(int id)
+        {
+            GetProductsRequest getProductsRequest = new GetProductsRequest { ProductId = id };
+            var result = productClient.GetProduct(getProductsRequest);
+
+            return result;
+        }
+
+        internal async Task<IList<ProductModel>> GetAllProducts()
+        {
+            GetAllRequest getAllRequest = new GetAllRequest();
+            var getAllReply = await productClient.GetAllProductsAsync(getAllRequest);
+            var result = getAllReply.Product as IList<ProductModel>;
+
+            return result;
+        }
+
+        internal async Task SearchProduct(string name)
+        {
+            var token = new CancellationTokenSource();
+            SearchRequest searchRequest = new SearchRequest { ProductName = name };
+            var searchreply = productClient.SearchProducts(searchRequest);
+
+            try
+            {
+                await foreach (var response in searchreply.ResponseStream.ReadAllAsync(token.Token))
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(1));
+
+                    Console.WriteLine(response.ProductName);
+
+
+                }
+                Console.WriteLine("Tüm ürünler çekildi.");
+
+                token.Cancel();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+    }  
+}
